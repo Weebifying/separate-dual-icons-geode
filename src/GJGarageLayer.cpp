1786d2798e106bf95c6c4eead9dbb5907e47c311
Weebifying <weebifylmao123@gmail.com>
2024-02-27 02:38:24 +0700

    outline for now

--- src/GJGarageLayer.cpp
+++ src/GJGarageLayer.cpp
@@ -2,7 +2,45 @@
 #include <Geode/modify/GJGarageLayer.hpp>
 
 
-class $modify(GJGarageLayer) {
+class $modify(MyGarageLayer, GJGarageLayer) {
+    void on2PToggle(CCObject* sender) {
+
+    }
+
+    bool init() {
+		if (!GJGarageLayer::init()) return false;
+
+        auto winSize = CCDirector::get()->getWinSize();
+
+        // auto toggler = CCMenuItemToggler::createWithStandardSprites(this, menu_selector(MyGarageLayer::on2PToggle), 1.f);
+
+        SimplePlayer* player1 = as<SimplePlayer*>(this->getChildByID("player-icon"));
+        player1->setPositionX(player1->getPositionX() - winSize.width/12);
+
+        auto player2 = SimplePlayer::create(0);
+        player2->setID("player2-icon");
+        player2->setScale(1.6f);
+        player2->setPosition(player1->getPosition());
+        player2->setPositionX(player2->getPositionX() + winSize.width/6);
+
+        player2->setColor(GameManager::get()->colorForIdx(PlayerData::player2Color1));
+        player2->setSecondColor(GameManager::get()->colorForIdx(PlayerData::player2Color2));
+
+        if (PlayerData::player2Glow) {
+            player2->setGlowOutline(GameManager::get()->colorForIdx(PlayerData::player2ColorGlow));
+        } else {
+            player2->disableGlowOutline();
+        }
+
+        player2->updatePlayerFrame(PlayerData::player2Cube, IconType::Cube);
+
+        this->addChild(player2);
+        
+
+        return true;
+    }
+
+
     void onSelect(CCObject* sender) {
         GJGarageLayer::onSelect(sender);
         
