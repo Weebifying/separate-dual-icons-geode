3eb8f00fb1a734f475e6a05511827ab83998cc1d
Weebifying <weebifylmao123@gmail.com>
2024-02-28 00:27:23 +0700

    everything in gjgaragelayer should be done

--- src/GJGarageLayer.cpp
+++ src/GJGarageLayer.cpp
@@ -7,12 +7,169 @@ IconType type;
 class $modify(MyGarageLayer, GJGarageLayer) {
     void on2PToggle(CCObject* sender) {
         auto btn = as<CCMenuItemToggler*>(sender);
-
         PlayerData::player2Selected = !btn->isToggled();
+
+        auto winSize = CCDirector::get()->getWinSize();
+
+        auto iconBar = getChildOfType<ListButtonBar>(this, 0);
+
+        auto menu = getChildOfType<CCMenu>(getChildOfType<ListButtonPage>(getChildOfType<ExtendedLayer>(getChildOfType<BoomScrollLayer>(iconBar, 0), 0), 0), 0);
+        CCMenu* menu2 = nullptr;
+        if (type == IconType::Special) 
+            menu2 = getChildOfType<CCMenu>(getChildOfType<ListButtonPage>(getChildOfType<ExtendedLayer>(getChildOfType<BoomScrollLayer>(getChildOfType<ListButtonBar>(iconBar, 0), 0), 0), 0), 0);
+
+        auto cursor1 = this->getChildByID("cursor-1");
+        auto cursor2 = this->getChildByID("cursor-2");
+
+        if (PlayerData::player2Selected) {
+            
+            int tag = 0;
+            int tag2 = 0;
+
+            switch (type) {
+                case IconType::Cube:
+                    tag = PlayerData::player2Cube;
+                    break;
+                case IconType::Ship:
+                    tag = PlayerData::player2Ship;
+                    break;
+                case IconType::Ball:
+                    tag = PlayerData::player2Roll;
+                    break;
+                case IconType::Ufo:
+                    tag = PlayerData::player2Bird;
+                    break;
+                case IconType::Wave:
+                    tag = PlayerData::player2Dart;
+                    break;
+                case IconType::Robot:
+                    tag = PlayerData::player2Robot;
+                    break;
+                case IconType::Spider:
+                    tag = PlayerData::player2Spider;
+                    break;
+                case IconType::Swing:
+                    tag = PlayerData::player2Swing;
+                    break;
+                case IconType::Jetpack:
+                    tag = PlayerData::player2Jetpack;
+                    break;
+                case IconType::Special:
+                    tag = PlayerData::player2Trail;
+                    tag2 = PlayerData::player2ShipTrail;
+                    break;
+                case IconType::DeathEffect:
+                    tag = PlayerData::player2Death;
+                    break;
+                default:
+                    break;
+            }
+
+            bool tagIsHere = false;
+            bool tag2IsHere = false;
+
+            // i cant use getChildByTag bc crashes lol
+            if (menu) {
+                CCArrayExt<CCMenuItemSpriteExtra*> children = menu->getChildren();
+                for (auto* child : children) {
+                    if (child->getTag() == tag) {
+                        tagIsHere = true;
+                        cursor1->setVisible(true);
+                        cursor1->setPosition({child->getPositionX() + winSize.width/2, child->getPositionY() + winSize.height/2});
+                    }
+                }
+                if (!tagIsHere) cursor1->setVisible(false);
+            }
+
+            if (menu2) {
+                CCArrayExt<CCMenuItemSpriteExtra*> children2 = menu2->getChildren();
+                for (auto* child : children2) {
+                    if (child->getTag() == tag2) {
+                        tag2IsHere = true;
+                        cursor2->setVisible(true);
+                        cursor2->setPosition({child->getPositionX() + winSize.width/2, child->getPositionY() + winSize.height/2});
+                    }
+                }
+                if (!tag2IsHere) cursor2->setVisible(false);
+            }
+        } else {
+            int tag = 0;
+            int tag2 = 0;
+
+            switch (type) {
+                case IconType::Cube:
+                    tag = GameManager::get()->getPlayerFrame();
+                    break;
+                case IconType::Ship:
+                    tag = GameManager::get()->getPlayerShip();
+                    break;
+                case IconType::Ball:
+                    tag = GameManager::get()->getPlayerBall();
+                    break;
+                case IconType::Ufo:
+                    tag = GameManager::get()->getPlayerBird();
+                    break;
+                case IconType::Wave:
+                    tag = GameManager::get()->getPlayerDart();
+                    break;
+                case IconType::Robot:
+                    tag = GameManager::get()->getPlayerRobot();
+                    break;
+                case IconType::Spider:
+                    tag = GameManager::get()->getPlayerSpider();
+                    break;
+                case IconType::Swing:
+                    tag = GameManager::get()->getPlayerSwing();
+                    break;
+                case IconType::Jetpack:
+                    tag = GameManager::get()->getPlayerJetpack();
+                    break;
+                case IconType::Special:
+                    tag = GameManager::get()->getPlayerStreak();
+                    tag2 = GameManager::get()->getPlayerShipFire();
+                    break;
+                case IconType::DeathEffect:
+                    tag = GameManager::get()->getPlayerDeathEffect();
+                    break;
+                default:
+                    break;
+            }
+
+            bool tagIsHere = false;
+            bool tag2IsHere = false;
+
+            // i cant use getChildByTag bc crashes lol
+            if (menu) {
+                CCArrayExt<CCMenuItemSpriteExtra*> children = menu->getChildren();
+                for (auto* child : children) {
+                    if (child->getTag() == tag) {
+                        tagIsHere = true;
+                        cursor1->setVisible(true);
+                        cursor1->setPosition({child->getPositionX() + winSize.width/2, child->getPositionY() + winSize.height/2});
+                    }
+                }
+                if (!tagIsHere) cursor1->setVisible(false);
+            }
+
+            if (menu2) {
+                CCArrayExt<CCMenuItemSpriteExtra*> children2 = menu2->getChildren();
+                for (auto* child : children2) {
+                    if (child->getTag() == tag2) {
+                        tag2IsHere = true;
+                        cursor2->setVisible(true);
+                        cursor2->setPosition({child->getPositionX() + winSize.width/2, child->getPositionY() + winSize.height/2});
+                    }
+                }
+                if (!tag2IsHere) cursor2->setVisible(false);
+            }
+        }
     }
 
     bool init() {
         PlayerData::player2Selected = false;
+        page = 0;
+        type = IconType::Cube;
+
 		if (!GJGarageLayer::init()) return false;
 
         auto winSize = CCDirector::get()->getWinSize();
@@ -174,34 +331,44 @@ class $modify(MyGarageLayer, GJGarageLayer) {
                 player2->updatePlayerFrame(n, type);
             }
 
+            
 
             switch (type) {
                 case IconType::Cube:
                     Mod::get()->setSettingValue<int64_t>("cube", n);
+                    player2->setScale(1.6f);
                     break;
                 case IconType::Ship:
                     Mod::get()->setSettingValue<int64_t>("ship", n);
+                    player2->setScale(1.6f);
                     break;
                 case IconType::Ball:
                     Mod::get()->setSettingValue<int64_t>("roll", n);
+                    player2->setScale(1.6f);
                     break;
                 case IconType::Ufo:
                     Mod::get()->setSettingValue<int64_t>("bird", n);
+                    player2->setScale(1.6f);
                     break;
                 case IconType::Wave:
                     Mod::get()->setSettingValue<int64_t>("dart", n);
+                    player2->setScale(1.6f);
                     break;
                 case IconType::Robot:
                     Mod::get()->setSettingValue<int64_t>("robot", n);
+                    player2->setScale(1.6f);
                     break;
                 case IconType::Spider:
                     Mod::get()->setSettingValue<int64_t>("spider", n);
+                    player2->setScale(1.6f);
                     break;
                 case IconType::Swing:
                     Mod::get()->setSettingValue<int64_t>("swing", n);
+                    player2->setScale(1.6f);
                     break;
                 case IconType::Jetpack:
                     Mod::get()->setSettingValue<int64_t>("jetpack", n);
+                    player2->setScale(1.5f);
                     break;
                 case IconType::Special:
                     if (as<CCNode*>(sender)->getParent()->getChildrenCount() == 7) {
