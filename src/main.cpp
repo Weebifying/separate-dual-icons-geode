c639f9746b71caa8f266863eee3f513b54e6ee69
Weebifying <weebifylmao123@gmail.com>
2024-03-15 02:53:22 +0700

    mac?????

--- src/main.cpp
+++ src/main.cpp
@@ -2,33 +2,23 @@
 #include <Geode/loader/SettingEvent.hpp>
 
 $on_mod(Loaded) {
-    gd::string types[] = {"cube", "ship", "roll", "bird", "dart", "robot", "spider", "swing", "jetpack", "trail", "death", "shiptrail"};
-    for (auto t : types) 
-        if (!Mod::get()->getSavedValue<int64_t>(t)) 
-            Mod::get()->setSavedValue<int64_t>(t, 1);
+    PlayerData::player2Cube = Mod::get()->getSavedValue<int64_t>("cube", 1);
+    PlayerData::player2Ship = Mod::get()->getSavedValue<int64_t>("ship", 1);
+    PlayerData::player2Roll = Mod::get()->getSavedValue<int64_t>("roll", 1);
+    PlayerData::player2Bird = Mod::get()->getSavedValue<int64_t>("bird", 1);
+    PlayerData::player2Dart = Mod::get()->getSavedValue<int64_t>("dart", 1);
+    PlayerData::player2Robot = Mod::get()->getSavedValue<int64_t>("robot", 1);
+    PlayerData::player2Spider = Mod::get()->getSavedValue<int64_t>("spider", 1);
+    PlayerData::player2Swing = Mod::get()->getSavedValue<int64_t>("swing", 1);
+    PlayerData::player2Jetpack = Mod::get()->getSavedValue<int64_t>("jetpack", 1);
 
-    gd::string colorTypes[] = {"color1", "color2", "colorglow"};
-    for (auto t : types) 
-        if (!Mod::get()->getSavedValue<int64_t>(t)) 
-            Mod::get()->setSavedValue<int64_t>(t, 0);
+    PlayerData::player2Color1 = Mod::get()->getSavedValue<int64_t>("color1", 0);
+    PlayerData::player2Color2 = Mod::get()->getSavedValue<int64_t>("color2", 0);
+    PlayerData::player2ColorGlow = Mod::get()->getSavedValue<int64_t>("colorglow", 0);
+    PlayerData::player2Glow = Mod::get()->getSavedValue<bool>("glow", false);
 
-    PlayerData::player2Cube = Mod::get()->getSavedValue<int64_t>("cube");
-    PlayerData::player2Ship = Mod::get()->getSavedValue<int64_t>("ship");
-    PlayerData::player2Roll = Mod::get()->getSavedValue<int64_t>("roll");
-    PlayerData::player2Bird = Mod::get()->getSavedValue<int64_t>("bird");
-    PlayerData::player2Dart = Mod::get()->getSavedValue<int64_t>("dart");
-    PlayerData::player2Robot = Mod::get()->getSavedValue<int64_t>("robot");
-    PlayerData::player2Spider = Mod::get()->getSavedValue<int64_t>("spider");
-    PlayerData::player2Swing = Mod::get()->getSavedValue<int64_t>("swing");
-    PlayerData::player2Jetpack = Mod::get()->getSavedValue<int64_t>("jetpack");
-
-    PlayerData::player2Color1 = Mod::get()->getSavedValue<int64_t>("color1");
-    PlayerData::player2Color2 = Mod::get()->getSavedValue<int64_t>("color2");
-    PlayerData::player2ColorGlow = Mod::get()->getSavedValue<int64_t>("colorglow");
-    PlayerData::player2Glow = Mod::get()->getSavedValue<bool>("glow");
-
-    PlayerData::player2Trail = Mod::get()->getSavedValue<int64_t>("trail");
-    PlayerData::player2Death = Mod::get()->getSavedValue<int64_t>("death");
-    PlayerData::player2ShipTrail = Mod::get()->getSavedValue<int64_t>("shiptrail");
-    PlayerData::lastType = Mod::get()->getSavedValue<int64_t>("lasttype");
+    PlayerData::player2Trail = Mod::get()->getSavedValue<int64_t>("trail", 1);
+    PlayerData::player2Death = Mod::get()->getSavedValue<int64_t>("death", 1);
+    PlayerData::player2ShipTrail = Mod::get()->getSavedValue<int64_t>("shiptrail", 1);
+    PlayerData::lastType = Mod::get()->getSavedValue<int64_t>("lasttype", 1);
 }
